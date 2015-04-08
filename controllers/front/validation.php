dfb2f8704788bc448b895f3c523e4ac9ba3bf497
Romain d'Alverny <rda@hupstream.com>
2015-04-08 13:33:40 +0200

    Reorg comment

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -35,7 +35,9 @@ class CashwayValidationModuleFrontController extends ModuleFrontController
 		if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
 			Tools::redirect('index.php?controller=order&step=1');
 
-		// Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
+		// Check that this payment option is still available
+		// in case the customer changed his address
+		// just before the end of the checkout process
 		$authorized = false;
 		foreach (Module::getPaymentModules() as $module)
 			if ($module['name'] == 'cashway')
