2c410eb294ff9c4ea36015c10646939b7907ab2b
Romain d'Alverny <rda@hupstream.com>
2015-09-15 18:28:55 +0200

    Adjust tests

--- tests/php/01_ModuleTest.php
+++ tests/php/01_ModuleTest.php
@@ -14,27 +14,29 @@ class CashWayModuleTest extends PHPUnit_Framework_TestCase
             'newOrderStatus' => $os,
             'id_order' => 1
         );
+
         $res = $cwmod->hookActionOrderStatusUpdate($params);
+        var_dump($res);
         $this->assertEquals('POST', $res['method']);
         $this->assertEquals('Basic '.base64_encode(TEST_KEY.':'.TEST_SECRET), $res['headers']['Authorization']);
         $this->assertEquals('/1/shops/me/events', $res['request']);
 
-        $body = json_decode($res['body'], true);
-        $this->assertEquals($body['event'], 'payment_failed');
-        $this->assertEquals($body['provider'], 'test_payment_method');
-        $this->assertEquals($body['order']['id'], 1);
-        $this->assertEquals($body['customer']['email'], 'test.customer@do.cshw.pl');
+        $body = json_decode($res['body']);
+        $this->assertEquals('payment_failed', $body->event);
+        $this->assertEquals('test_payment_method', $body->provider);
+        $this->assertEquals(1, $body->order->id);
+        $this->assertEquals('test.customer@do.cshw.pl', $body->customer->email);
     }
 
     public function testUpdateNotificationParameters()
     {
         $cwmod = new CashWay();
         $res = $cwmod->updateNotificationParameters();
-        $this->assertEquals($res['headers']['Authorization'], 'Basic '.base64_encode(TEST_KEY.':'.TEST_SECRET));
-        $this->assertEquals($res['method'], 'POST');
-        $this->assertEquals($res['request'], '/1/shops/me');
+        $this->assertEquals('POST', $res['method']);
+        $this->assertEquals('Basic '.base64_encode(TEST_KEY.':'.TEST_SECRET), $res['headers']['Authorization']);
+        $this->assertEquals('/1/shops/me', $res['request']);
 
-        $body = json_decode($res['body'], true);
-        $this->assertEquals($body['notification_url'], 'scheme://host.tld/cashway/notification');
+        $body = json_decode($res['body']);
+        $this->assertEquals('scheme://host.tld/cashway/notification', $body->notification_url);
     }
 }
