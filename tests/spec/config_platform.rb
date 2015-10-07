13f23723e9ed1194a33affa615a8df1619fc021d
Jean-Baptiste Pacaud-Paris <jbpacaud@cashway.fr>
2015-10-07 18:19:21 +0200

    Use ENV-defined values

--- tests/spec/config_platform.rb
+++ tests/spec/config_platform.rb
@@ -3,20 +3,17 @@ require 'spec_helper'
 describe "Installation de la boutique PrestaShop " do
 
   it "charge la page d'admin" do
-    session.visit 'http://localhost:8082/prestashop/install'
+    session.visit ENV['ADMIN_PATH']
     find('#btNext').click
     find('#set_license').click
     find('#btNext').click
     fill_in 'infosShop', :with => 'test'
-    fill_in 'infosFirstname', :with => 'Jean-Baptiste'
-    fill_in 'infosName', :with => 'Pacaud-Paris'
-    fill_in 'infosEmail', :with => 'testpacaud@gmail.com'
-    fill_in 'infosPassword', :with => 'cashwaytest'
-    fill_in 'infosPasswordRepeat', :with => 'cashwaytest'
+    fill_in 'infosFirstname', :with => ENV['CUSTOMER_FIRSTNAME']
+    fill_in 'infosName', :with => ENV['CUSTOMER_LASTNAME']
+    fill_in 'infosEmail', :with => ENV['CUSTOMER_EMAIL']
+    fill_in 'infosPassword', :with => ENV['CUSTOMER_PASSWD']
+    fill_in 'infosPasswordRepeat', :with => ENV['CUSTOMER_PASSWD']
     find('#btNext').click
     find('#btNext').click
-    $stdin.gets
   end
-
-
 end
