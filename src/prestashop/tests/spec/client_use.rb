37ae1081cf10d155432e404d4525cab8f611b08a
Romain d'Alverny <rdalverny@cashway.fr>
2016-03-22 14:28:07 +0100

    PS/tests: add an option to not verify payment page texts
    This allows to faster validate the module on a functional level,
    without playing with exact text strings & translations.

--- src/prestashop/tests/spec/client_use.rb
+++ src/prestashop/tests/spec/client_use.rb
@@ -31,23 +31,26 @@ end
 
     it "checks shipping/billing addresses" do
       find("button[name=processAddress]").click
-      expect(page).to have_content('terms of service')
+      expect(page).to have_content('terms of service') if ENV.fetch('VERIFY_CONTENT', 'no') == 'yes'
     end
 
     it "accepts terms & conditions, confirms order" do
       expect(page).to have_selector('#uniform-cgv')
       find('label[for=cgv]').click
       find(:xpath, '//button[@class="button btn btn-default standard-checkout button-medium"]').click
-      expect(page).to have_content ENV['MODULE_PAY_ACTION_TEXT']
-      expect(page).to have_content 'Continue shopping'
-      expect(page).to have_content 'CHOOSE YOUR PAYMENT METHOD'
+
+      if ENV.fetch('VERIFY_CONTENT', 'no') == 'yes'
+        expect(page).to have_content ENV['MODULE_PAY_ACTION_TEXT']
+        expect(page).to have_content 'Continue shopping'
+        expect(page).to have_content 'CHOOSE YOUR PAYMENT METHOD'
+      end
     end
 
     it "selects our #{ENV['MODULE_NAME']} payment solution" do
       find('a.' + ENV['MODULE_NAME'].downcase).click
     end
 
-    if 2500 == price
+    if 2500 == price && ENV.fetch('VERIFY_CONTENT', 'no') == 'yes'
       it "confirms that is is blocked" do
         expect(page).to have_content 'Hélas, vous avez dépassé le montant maximum possible d\'achats via CashWay sur la période des 12 derniers mois (plus d’informations).'
       end
@@ -58,14 +61,16 @@ end
       it "confirms CashWay transaction" do
         fail "Le service ne fonctionne pas..." if page.has_content? 'Hélas'
 
-        expect(page).to have_content 'Order total'
-        expect(page).to have_content 'Service fees'
-        expect(page).to have_content 'Total amount to pay'
-        expect(page).to have_content 'you confirm you have read and agreed to'
-        expect(page).to have_content 'Dealers near you'
+        if ENV.fetch('VERIFY_CONTENT', 'no') == 'yes'
+          expect(page).to have_content 'Order total'
+          expect(page).to have_content 'Service fees'
+          expect(page).to have_content 'Total amount to pay'
+          expect(page).to have_content 'you confirm you have read and agreed to'
+          expect(page).to have_content 'Dealers near you'
 
-        if 250 == price
-          #expect(page).to have_content 'pour encaisser ce montant, la réglementation française nous impose de contrôler votre identité.'
+          if 250 == price
+            #expect(page).to have_content 'pour encaisser ce montant, la réglementation française nous impose de contrôler votre identité.'
+          end
         end
 
         find('#cashway-confirm-btn').click
