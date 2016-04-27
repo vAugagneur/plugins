3367ec0e90c43c704891fee6e5a278db69537636
bdurand2 <bdurand@cashway.fr>
2016-04-27 12:55:59 +0200

    WC: Refactor client use cases
    The client use cases have been actualized to match the recent modifications of the plugin.

--- src/woocommerce/tests/spec/client_use_2500e.rb
+++ src/woocommerce/tests/spec/client_use_2500e.rb
@@ -11,46 +11,20 @@ describe "Test d'une commande > 2500 € sur " + ENV['TEST_SERVER'] do
     find(:xpath, '//a[text()="Click here to login"]').click
     fill_in 'username', :with => ENV['CUSTOMER_FIRSTNAME']
     fill_in 'password', :with => ENV['CUSTOMER_PASSWD']
-    find(:xpath, '//input[@value="Login"]').click
-    $stdin.gets
+		find(:xpath, '//input[@value="Login"]').click
+		fill_in 'billing_first_name', :with => ENV['CUSTOMER_FIRSTNAME']
+		fill_in 'billing_last_name', :with => ENV['CUSTOMER_LASTNAME']
+		fill_in 'billing_email', :with => ENV['CUSTOMER_EMAIL']
+		fill_in 'billing_phone', :with => ENV['CUSTOMER_PHONE']
+		fill_in 'billing_address_1', :with => ENV['CUSTOMER_ADDRESS']
+		fill_in 'billing_phone', :with => ENV['CUSTOMER_PHONE']
+		fill_in 'billing_postcode', :with => ENV['CUSTOMER_ZIPCODE']
+		fill_in 'billing_city', :with => ENV['CUSTOMER_CITY']
 	end
 
 	it "passe commande" do
-		session.click_link_or_button 'Proceed to checkout'
+		find(:xpath, '//input[@value="Place order"]').click
+		expect(page).to have_content 'Votre commande dépasse la limite de montant pour le paiement avec Cashway'
 	end
 
-	it "s'identifie" do
-    find('#email').set ENV['CUSTOMER_EMAIL']
-		find('#passwd').set ENV['CUSTOMER_PASSWD']
-		find_button('SubmitLogin').click
-		expect(page).to have_content ENV['CUSTOMER_NAME']
-	end
-
-	it "retourne au panier" do
-		visit '/index.php?controller=order'
-    find('a', text: 'Proceed to checkout').click
-	end
-
-  it "passe les adresses" do
-		find("button[name=processAddress]").click
-		expect(page).to have_content('terms of service')
-	end
-
-	it "valide les CGV et confirme" do
-		expect(page).to have_selector('#uniform-cgv')
-		find('label[for=cgv]').click
-		session.click_button 'Proceed to checkout'
-		expect(page).to have_content 'Payment'
-		expect(page).to have_content ENV['MODULE_PAY_ACTION_TEXT']
-		expect(page).to have_content 'Continue shopping'
-		expect(page).to have_content 'CHOOSE YOUR PAYMENT METHOD'
-	end
-
-	it "choisit l'option " + ENV['MODULE_NAME'] do
-		find('a.' + ENV['MODULE_NAME'].downcase).click
-	end
-
-	it "vérifie l'impossibilité de continuer" do
-		expect(page).to have_content 'Hélas, vous avez dépassé le montant maximum possible d\'achats via CashWay sur la période des 12 derniers mois (plus d’informations).'
-	end
 end
--- src/woocommerce/tests/spec/client_use_50.rb
+++ src/woocommerce/tests/spec/client_use_50.rb
@@ -12,70 +12,24 @@ describe "Test d'une commande < 100 € sur " + ENV['TEST_SERVER'] do
     fill_in 'username', :with => ENV['CUSTOMER_FIRSTNAME']
     fill_in 'password', :with => ENV['CUSTOMER_PASSWD']
     find(:xpath, '//input[@value="Login"]').click
-    $stdin.gets
+		fill_in 'billing_first_name', :with => ENV['CUSTOMER_FIRSTNAME']
+		fill_in 'billing_last_name', :with => ENV['CUSTOMER_LASTNAME']
+		fill_in 'billing_email', :with => ENV['CUSTOMER_EMAIL']
+		fill_in 'billing_phone', :with => ENV['CUSTOMER_PHONE']
+		fill_in 'billing_address_1', :with => ENV['CUSTOMER_ADDRESS']
+		fill_in 'billing_phone', :with => ENV['CUSTOMER_PHONE']
+		fill_in 'billing_postcode', :with => ENV['CUSTOMER_ZIPCODE']
+		fill_in 'billing_city', :with => ENV['CUSTOMER_CITY']
 	end
 
-	it "passe commande" do
-		session.click_link_or_button 'Proceed to checkout'
+	it "passe commande et est redirigé sur front app" do
+		find(:xpath, '//input[@value="Place order"]').click
 	end
 
-	it "s'identifie" do
-		find('#email').set ENV['CUSTOMER_EMAIL']
-		find('#passwd').set ENV['CUSTOMER_PASSWD']
-		find_button('SubmitLogin').click
-		expect(page).to have_content ENV['CUSTOMER_NAME']
+	it "Effectue un retour sur le site marchand à partir de front app" do
+		# TODO This is a test link, replace with real one
+		find('#confirm_and_back_to_shop').click
+		expect(page).to have_content "Merci d'avoir commandé avec CashWay !"
 	end
 
-	it "retourne au panier" do
-		visit '/index.php?controller=order'
-		find('a', text: 'Proceed to checkout').click
-	end
-
-  it "passe les adresses" do
-		find("button[name=processAddress]").click
-    expect(page).to have_content('terms of service')
-	end
-
-	it "valide les CGV et confirme" do
-		expect(page).to have_selector('#uniform-cgv')
-		find('label[for=cgv]').click
-		find(:xpath, '//button[@class="button btn btn-default standard-checkout button-medium"]').click
-		expect(page).to have_content ENV['MODULE_PAY_ACTION_TEXT']
-		expect(page).to have_content 'Continue shopping'
-		expect(page).to have_content 'CHOOSE YOUR PAYMENT METHOD'
-	end
-
-	it "choisit l'option " + ENV['MODULE_NAME'] do
-		find('a.' + ENV['MODULE_NAME'].downcase).click
-	end
-
-	it "confirme la commande avec CashWay, constate une alerte KYC" do
-		fail "Le service ne fonctionne pas..." if page.has_content? 'Hélas'
-		expect(page).to have_content 'Total à payer au buraliste'
-		expect(page).to have_content 'Please confirm your order by clicking'
-		expect(page).to have_content 'J\'ai lu les conditions d’utilisation de CashWay et j’y adhère sans réserve'
-		expect(page).to have_content 'Les distributeurs proches de chez vous'
-		#session.click_button 'Je confirme ma commande'
-		find('label[for=cgu-accept]').click
-		find('#cashway-confirm-btn').click
-	end
-
-	it "consulte la marche à suivre, constate liens vers formulaire KYC" do
-		#expect(page).to have_content 'Confirmation de commande'
-		expect(page).to have_content 'rendez-vous dans un des points de paiement indiqués sur notre carte, muni du code suivant'
-		expect(page).to have_content 'Please note and keep your order reference'
-		barcode = find('#cashway-barcode-label').text
-		#expect(barcode[0..6]).to eq '3663538'
-
-		#mail = URI.parse(page.find('a[id = "cashway-kyc-email"]')['href'])
-		#expect(mail.to).to eq 'validation@cashway.fr'
-
-		#form = URI.parse(page.find('a[id = "cashway-kyc-form"]')['href'])
-		#expect(form.scheme).to eq 'https'
-		#expect(form.host.end_with?('.cashway.fr')).to be true
-
-		# TODO, ajouter les expect() pour les liens vers mail & formulaire KYC
-		# TODO télécharger/imprimer ticket de paiement
-		# TODO liste texte des bureaux de paiement
-	end
 end
