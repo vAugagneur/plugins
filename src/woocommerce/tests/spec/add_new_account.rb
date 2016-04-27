c3d727f506d8a8ab03a005695394f48fd3b260a9
bdurand2 <bdurand@cashway.fr>
2016-04-27 12:52:39 +0200

    WC: Modify add account test with env
    The add account test now uses the environment variables.

--- src/woocommerce/tests/spec/add_new_account.rb
+++ src/woocommerce/tests/spec/add_new_account.rb
@@ -15,13 +15,13 @@ describe "Adds new customer account" do
     find('#menu-users').click
     find(:xpath, "//a[@href='user-new.php']").click
     fill_in 'user_login', :with => ENV['CUSTOMER_FIRSTNAME']
-    fill_in 'email', :with => 'testapacaud@gmail.com'
+    fill_in 'email', :with => 'anne.de.bretagne@do.cshw.pl'
     find(:xpath, '//button[@class="button button-secondary wp-generate-pw hide-if-no-js"]').click
     fill_in 'pass1-text', :with => ENV['CUSTOMER_PASSWD']
     fill_in 'pass1-text', :with => ENV['CUSTOMER_PASSWD']
     find(:xpath, '//input[@name="pw_weak"]').click
     find(:xpath, '//input[@id="createusersub"]').click
-    find(:xpath, '//a[text()="Jean-Baptiste"]').click
+    find(:xpath, '//a[text()="Anne"]').click
     fill_in 'billing_address_1', :with => ENV['CUSTOMER_ADRESS']
     fill_in 'billing_city', :with => ENV['CUSTOMER_CITY']
     fill_in 'billing_postcode', :with => ENV['CUSTOMER_ZIPCODE']
