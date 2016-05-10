f710efa0464bc6e00f0f42a319d283511fac288d
bdurand2 <bdurand@cashway.fr>
2016-05-10 10:39:49 +0200

    MA: Add tests for the new fee levels
    The new fee levels have been integrated to the fees calculation test.

--- src/magento/tests/spec/test_fees.rb
+++ src/magento/tests/spec/test_fees.rb
@@ -3,92 +3,162 @@ require 'spec_helper'
 describe "Test the right calculation of the fees" do
 
   it "Logs in to the customer's account" do
-      session.visit('/')
-      find(:xpath, '//a[@data-target-element="#header-account"]').click
-      find(:xpath, '//a[@title="Log In"]').click
-      first('#email').set ENV['CUSTOMER_EMAIL']
-      fill_in 'pass', :with => ENV['CUSTOMER_PASSWD']
-      find('#send2').click
+    session.visit('/')
+    find(:xpath, '//a[@data-target-element="#header-account"]').click
+    find(:xpath, '//a[@title="Log In"]').click
+    first('#email').set ENV['CUSTOMER_EMAIL']
+    fill_in 'pass', :with => ENV['CUSTOMER_PASSWD']
+    find('#send2').click
   end
 
   it "Orders one 20 euros product" do
-      sleep(1)
-      find('#search').set '_20'
-      click_link_or_button 'Search'
-      first(:xpath, '//button[@class="button btn-cart"]').click
-      expect(page).to have_content 'SHOPPING CART'
-      first(:xpath, '//button[@title="Proceed to Checkout"]').click
-      expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
-      find(:xpath, '//button[@onclick="billing.save()"]').click
-      sleep(1)
-      find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
-      sleep(1)
-      choose "Cashway payment"
-      expect(page).to have_content "A fee of 1"
-      visit('/index.php/checkout/cart')
-      sleep(1)
-      first(:xpath, '//a[@title="Remove Item"]').click
+    sleep(1)
+    find('#search').set '_20'
+    click_link_or_button 'Search'
+    first(:xpath, '//button[@class="button btn-cart"]').click
+    expect(page).to have_content 'SHOPPING CART'
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 1"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//a[@title="Remove Item"]').click
   end
 
   it "Orders two 50 euros product" do
-      sleep(1)
-      find('#search').set '_50'
-      click_link_or_button 'Search'
-      first(:xpath, '//button[@class="button btn-cart"]').click
-      expect(page).to have_content 'SHOPPING CART'
-      first(:xpath, '//input[@title="Qty"]').click
-      first(:xpath, '//input[@title="Qty"]').set '2'
-      find(:xpath, '//button[@title="Update"]').click
-      sleep(1)
-      first(:xpath, '//button[@title="Proceed to Checkout"]').click
-      expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
-      find(:xpath, '//button[@onclick="billing.save()"]').click
-      sleep(1)
-      find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
-      sleep(1)
-      choose "Cashway payment"
-      expect(page).to have_content "A fee of 2"
-      visit('/index.php/checkout/cart')
-      sleep(1)
-      first(:xpath, '//input[@title="Qty"]').click
-      first(:xpath, '//input[@title="Qty"]').set '4'
-      find(:xpath, '//button[@title="Update"]').click
+    sleep(1)
+    find('#search').set '_50'
+    click_link_or_button 'Search'
+    first(:xpath, '//button[@class="button btn-cart"]').click
+    expect(page).to have_content 'SHOPPING CART'
+    first(:xpath, '//input[@title="Qty"]').click
+    first(:xpath, '//input[@title="Qty"]').set '2'
+    sleep(1)
+    find(:xpath, '//button[@title="Update"]').click
+    sleep(1)
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 2"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//input[@title="Qty"]').click
+    first(:xpath, '//input[@title="Qty"]').set '4'
+    sleep(1)
+    find(:xpath, '//button[@title="Update"]').click
   end
 
   it "Orders four 50 euros product" do
-      sleep(1)
-      first(:xpath, '//button[@title="Proceed to Checkout"]').click
-      expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
-      find(:xpath, '//button[@onclick="billing.save()"]').click
-      sleep(1)
-      find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
-      sleep(1)
-      choose "Cashway payment"
-      expect(page).to have_content "A fee of 3"
-      visit('/index.php/checkout/cart')
-      sleep(1)
-      first(:xpath, '//a[@title="Remove Item"]').click
+    sleep(1)
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 3"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//input[@title="Qty"]').click
+    first(:xpath, '//input[@title="Qty"]').set '6'
+    sleep(1)
+    find(:xpath, '//button[@title="Update"]').click
   end
 
-  it "Orders two 250 euros product" do
-      sleep(1)
-      find('#search').set '_250'
-      click_link_or_button 'Search'
-      first(:xpath, '//button[@class="button btn-cart"]').click
-      expect(page).to have_content 'SHOPPING CART'
-      first(:xpath, '//input[@title="Qty"]').click
-      first(:xpath, '//input[@title="Qty"]').set '2'
-      find(:xpath, '//button[@title="Update"]').click
-      first(:xpath, '//button[@title="Proceed to Checkout"]').click
-      expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
-      find(:xpath, '//button[@onclick="billing.save()"]').click
-      sleep(1)
-      find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
-      sleep(1)
-      choose "Cashway payment"
-      expect(page).to have_content "A fee of 4"
-      visit('/index.php/checkout/cart')
-      sleep(1)
-      first(:xpath, '//a[@title="Remove Item"]').click
+  it "Orders six 50 euros product" do
+    sleep(1)
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 4"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//input[@title="Qty"]').click
+    first(:xpath, '//input[@title="Qty"]').set '10'
+    sleep(1)
+    find(:xpath, '//button[@title="Update"]').click
+  end
+
+  it "Orders ten 50 euros products" do
+    sleep(1)
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 5"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//input[@title="Qty"]').click
+    first(:xpath, '//input[@title="Qty"]').set '15'
+    sleep(1)
+    find(:xpath, '//button[@title="Update"]').click
+  end
+
+  it "Orders fifteen 50 euros products" do
+    sleep(1)
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 6"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//input[@title="Qty"]').click
+    first(:xpath, '//input[@title="Qty"]').set '17'
+    sleep(1)
+    find(:xpath, '//button[@title="Update"]').click
+  end
+
+  it "Orders seventeen 50 euros products" do
+    sleep(1)
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 7"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//input[@title="Qty"]').click
+    first(:xpath, '//input[@title="Qty"]').set '19'
+    sleep(1)
+    find(:xpath, '//button[@title="Update"]').click
+  end
+
+  it "Orders nineteen 50 euros products" do
+    sleep(1)
+    first(:xpath, '//button[@title="Proceed to Checkout"]').click
+    expect(page).to have_content 'YOUR CHECKOUT PROGRESS'
+    find(:xpath, '//button[@onclick="billing.save()"]').click
+    sleep(1)
+    find(:xpath, '//button[@onclick="shippingMethod.save()"]').click
+    sleep(1)
+    choose "Cashway payment"
+    expect(page).to have_content "A fee of 8"
+    visit('/index.php/checkout/cart')
+    sleep(1)
+    first(:xpath, '//a[@title="Remove Item"]').click
   end
 end
